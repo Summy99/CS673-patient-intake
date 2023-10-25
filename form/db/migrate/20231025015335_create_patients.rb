c5903ca453dbd89f624e02a03df8bf95373317ce
Summy99 <70970124+Summy99@users.noreply.github.com>
2023-10-24 22:25:56 -0400

    updating migration file to contain all fields

--- form/db/migrate/20231025015335_create_patients.rb
+++ form/db/migrate/20231025015335_create_patients.rb
@@ -2,8 +2,18 @@ class CreatePatients < ActiveRecord::Migration[7.0]
   def change
     create_table :patients do |t|
       t.string :first_name
+      t.array :middle_name
       t.string :last_name
       t.string :gender
+      t.date :dob
+      t.integer :ssn
+
+      t.string :phone_number
+      t.string :email
+
+      t.array :emergency_contacts
+
+      t.array :medical_history
 
       t.timestamps
     end
