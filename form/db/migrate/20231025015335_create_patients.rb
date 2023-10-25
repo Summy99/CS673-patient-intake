7413f44a5d4eca3c9c7718f57e01dde3e833a1e5
Summy99 <70970124+Summy99@users.noreply.github.com>
2023-10-24 22:35:31 -0400

    Can't put arrays in the table so changed fields to reflect

--- form/db/migrate/20231025015335_create_patients.rb
+++ form/db/migrate/20231025015335_create_patients.rb
@@ -2,7 +2,7 @@ class CreatePatients < ActiveRecord::Migration[7.0]
   def change
     create_table :patients do |t|
       t.string :first_name
-      t.array :middle_name
+      t.string :middle_name
       t.string :last_name
       t.string :gender
       t.date :dob
@@ -10,10 +10,6 @@ class CreatePatients < ActiveRecord::Migration[7.0]
 
       t.string :phone_number
       t.string :email
-
-      t.array :emergency_contacts
-
-      t.array :medical_history
 
       t.timestamps
     end
