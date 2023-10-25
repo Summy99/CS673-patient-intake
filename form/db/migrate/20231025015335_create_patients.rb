3c649d6833b7e75a8f755674ad0565cd72e5acb3
Summy99 <70970124+Summy99@users.noreply.github.com>
2023-10-24 22:04:00 -0400

    Created "Patient" model, also created gitignore with tmp/ and log/ ignored

---
+++ form/db/migrate/20231025015335_create_patients.rb
@@ -0,0 +1,11 @@
+class CreatePatients < ActiveRecord::Migration[7.0]
+  def change
+    create_table :patients do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :gender
+
+      t.timestamps
+    end
+  end
+end
