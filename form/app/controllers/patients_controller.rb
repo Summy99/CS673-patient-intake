7655b98bad11318576839ec608583c96ce6f0c76
Summy99 <70970124+Summy99@users.noreply.github.com>
2023-10-25 15:15:16 -0400

    basic form created

--- form/app/controllers/patients_controller.rb
+++ form/app/controllers/patients_controller.rb
@@ -6,6 +6,20 @@ class PatientsController < ApplicationController
     @patients = Patient.all
   end
 
+  def new # Method that is called when new view is rendered
+    @patient = Patient.new
+  end
+
+  def create # Method that is called when a patient is created
+    @patient = Patient.new(patient_params)
+
+    if @patient.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def age(id) # Method that calculates the age of the patient
     now = Date.today
     patient = Patient.find(id)
@@ -15,4 +29,10 @@ class PatientsController < ApplicationController
   end
 
   helper_method :age # Makes the age method available to the view
+
+
+  private
+    def patient_params
+      params.require(:patient).permit(:first_name, :middle_name, :last_name, :gender, :dob, :ssn, :phone_number, :email)
+    end
 end
