7a7a1f45e3bb103880c9ba5d53d62e0b68136c8f
Zen <jchavan010@gmail.com>
2023-12-12 23:50:23 -0500

    added routes.rb

--- form/config/routes.rb
+++ form/config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
   root "patients#index"
 
-  resources :patients
+    resources :patients do
+      resources :contact_numbers, only: [:index, :create, :destroy]
+      resources :email_addresses, only: [:index, :create, :destroy]
+      resources :emergency_contact_people, only: [:index, :show, :create, :update, :destroy] do
+        resources :aux_contact_numbers, only: [:index, :create, :destroy]
+        resources :aux_email_ids, only: [:index, :create, :destroy]
+      end
+      resources :medical_histories, only: [:index, :create, :update, :destroy]
+      resources :medications, only: [:index, :create, :update, :destroy]
+      resources :treatments, only: [:index, :create, :update, :destroy]
+    end
+
 end
