bfa081919c4fd70db0f7c1b2f2d30a6aee788755
Dortiz246 <ortizdarryl@gmail.com>
2023-12-12 23:03:46 -0500

    fixed interaction between models and controller with new email and number models

--- form/app/controllers/patients_controller.rb
+++ form/app/controllers/patients_controller.rb
@@ -1,6 +1,11 @@
 require 'date'
 
 class PatientsController < ApplicationController
+  has_many :contact_numbers
+  has_many :email_addresses
+
+  accepts_nested_attributes_for :contact_numbers, allow_destroy: true
+  accepts_nested_attributes_for :email_addresses, allow_destroy: true
 
   def index # Method that is called when idex view is rendered
     @patients = Patient.all
@@ -8,13 +13,15 @@ class PatientsController < ApplicationController
 
   def new # Method that is called when new view is rendered
     @patient = Patient.new
+    @patient.contact_numbers.build
+    @patient.email_addresses.build
   end
 
   def create # Method that is called when a patient is created
     @patient = Patient.new(patient_params)
 
     if @patient.save
-      redirect_to root_path
+      redirect_to root_path, notice: "Patient created successfully"
     else
       render :new, status: :unprocessable_entity
     end
@@ -49,6 +56,6 @@ class PatientsController < ApplicationController
 
   private
     def patient_params
-      params.require(:patient).permit(:first_name, :middle_name, :last_name, :gender, :dob, :ssn, :phone_number, :email)
+      params.require(:patient).permit(:first_name, :middle_name, :last_name, :gender, :dob, :ssn, contact_numbers_attributes: [:number], email_addresses_attributes: [:email])
     end
 end
